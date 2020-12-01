fb9571f1bcc0cf77b914efdffe1fdb153a2f346f
Elmer <eadlpl11@gmail.com>
2020-12-01 11:54:08 -0700

    redirect success

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests as api
 import json
 import os
 import json
-
+import time
 
 def clear(): os.system('clear')
 clear()
@@ -26,32 +26,49 @@ def anadir():
 		print(x)
 
 	if request.method == "POST":
-		
-		poke_id0 = request.form["poke0"]
-		poke_id1 = request.form["poke1"]
-		poke_id2 = request.form["poke2"]
-		poke_id3 = request.form["poke3"]
-		poke_id4 = request.form["poke4"]
-		poke_id5 = request.form["poke5"]
-		
-		user_name = request.form["user_name"]
-		
-	for z in range (0,6):
-		print("Poke id "+poke_id)
-	
-	print("Poke id "+poke_id1)
-	print("Poke id "+poke_id2)
-	print("Poke id "+poke_id3)
-	print("Poke id "+poke_id4)
-	print("Poke id "+poke_id5)
+		try:
 
-	print("usr name "+user_name)
+			poke_id0 = request.form["poke0"]
+			poke_id1 = request.form["poke1"]
+			poke_id2 = request.form["poke2"]
+			poke_id3 = request.form["poke3"]
+			poke_id4 = request.form["poke4"]
+			poke_id5 = request.form["poke5"]
+			user_name = request.form["user_name"]
 
-	variable1 = api.get(f"https://pokeapi.co/api/v2/pokemon-form/"+poke_id1+"/").json()
-	variable2 = api.get(f"https://pokeapi.co/api/v2/pokemon-form/"+poke_id2+"/").json()
-	print(variable1["pokemon"]["name"])
-	print(variable2["pokemon"]["name"])
-	
+			form = {
+				"poke_id0" : poke_id0,
+				"poke_id1" : poke_id1,
+				"poke_id2" : poke_id2,
+				"poke_id3" : poke_id3,
+				"poke_id4" : poke_id4,
+				"poke_id5" : poke_id5,
+				"user_name" : request.form["user_name"]
+			}
+
+			if '' in form.values():
+				return render_template("añadir.html", pokemons = imagenes)
+
+			print("usr name "+user_name)
+
+			variable0 = api.get(f"https://pokeapi.co/api/v2/pokemon-form/"+poke_id0+"/").json()
+			variable1 = api.get(f"https://pokeapi.co/api/v2/pokemon-form/"+poke_id1+"/").json()
+			variable2 = api.get(f"https://pokeapi.co/api/v2/pokemon-form/"+poke_id2+"/").json()
+			variable3 = api.get(f"https://pokeapi.co/api/v2/pokemon-form/"+poke_id3+"/").json()
+			variable4 = api.get(f"https://pokeapi.co/api/v2/pokemon-form/"+poke_id4+"/").json()
+			variable5 = api.get(f"https://pokeapi.co/api/v2/pokemon-form/"+poke_id5+"/").json()
+
+			print(variable0["pokemon"]["name"])
+			print(variable1["pokemon"]["name"])
+			print(variable2["pokemon"]["name"])
+			print(variable3["pokemon"]["name"])
+			print(variable4["pokemon"]["name"])
+			print(variable5["pokemon"]["name"])
+
+			return  redirect("/success")
+		except:
+			return  render_template("añadir.html",pokemons = imagenes)
+		
 	return render_template("añadir.html", pokemons = imagenes)
 
 @app.route('/home')
@@ -81,6 +98,14 @@ def configurar():
 @app.route('/profile')
 def perfil():
     return render_template("perfil.html")
+
+@app.route('/success')
+def success():
+	return render_template("exito.html")
+	
+@app.route('/fail')
+def fail():
+    return cosa()
 
 
 if __name__ == "__main__":
